9dfc487a5f2fd8e5780f700a97a12afbe51580f9
haziq-danial <haziqdanial991@gmail.com>
2019-12-09 17:02:43 +0800

    added custom drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,125 +3,166 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MyPage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MyPage());
   }
 }
 
-
 class MyPage extends StatefulWidget {
-
   @override
   _MyPageState createState() => _MyPageState();
 }
 
-class _MyPageState extends State<MyPage> {
-
+class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   String _stringName = '';
+  bool _isCollapse = true;
 
   void _showDialog(BuildContext context, String stringName) {
-     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('test dialog'),
-          content: Text(stringName),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      }
-    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('test dialog'),
+            content: Text(stringName),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Close'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: CustomDrawer(),
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Container(
-          child: Builder(
-              builder: (context) => Form(
-              key: _formKey,
-              child: Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20.0
+    final _screenSize = MediaQuery.of(context).size;
+    final _screenHeight = _screenSize.height;
+    final _screenWidth = _screenSize.width;
+
+    return Stack(children: <Widget>[
+      CustomDrawer(),
+      AnimatedPositioned(
+        duration: Duration(milliseconds: 300),
+        top: 0,
+        bottom: 0,
+        right: _isCollapse ? 0 : -0.2 * _screenWidth,
+        left: _isCollapse ? 0 : 0.7 * _screenWidth,
+        curve: Curves.ease,
+        child: Material(
+          elevation: 8.0,
+          child: Scaffold(
+            body: Container(
+                child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 10, 20),
+                    child: SafeArea(
+                      child: Row(
+                        children: <Widget>[
+                          RawMaterialButton(
+                            elevation: 0,
+                            shape: CircleBorder(),
+                            child: Icon(
+                              Icons.menu,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isCollapse = !_isCollapse;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Column(
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter String',
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Enter a string';
-                          } else {
-                            return null;
-                          }
-                        },
-                        onSaved: (val) => setState(() => _stringName = val),
-                        onTap: () {
-                          final form = _formKey.currentState;
-                          form.reset();
-                          FocusScope.of(context).unfocus();
-                        },
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10.0
-                            ),
-                            child: RaisedButton(
-                              color: Colors.green,
-                              shape: StadiumBorder(),
-                              child: Text(
-                                  'Submit',
-                                  style: TextStyle(
-                                    color: Colors.white
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Builder(
+                          builder: (context) => Form(
+                            key: _formKey,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Column(
+                                children: <Widget>[
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Enter String',
+                                    ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'Enter a string';
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                    onSaved: (val) =>
+                                        setState(() => _stringName = val),
+                                    onTap: () {
+                                      final form = _formKey.currentState;
+                                      form.reset();
+                                      FocusScope.of(context).unfocus();
+                                    },
                                   ),
-                                ),
-                              onPressed: () {
-                                final form = _formKey.currentState;
-                                if (form.validate()) {
-                                  form.save();
-                                  _showDialog(context, _stringName);
-                                }
-                              },
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10.0),
+                                        child: RaisedButton(
+                                          color: Colors.green,
+                                          shape: StadiumBorder(),
+                                          child: Text(
+                                            'Submit',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          onPressed: () {
+                                            final form = _formKey.currentState;
+                                            if (form.validate()) {
+                                              form.save();
+                                              _showDialog(context, _stringName);
+                                            }
+                                          },
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          )
-                        ],
-                      )
+                          ),
+                        ),
+                      ),
                     ],
                   ),
-                ),
+                ],
               ),
-            ),
+            )),
           ),
         ),
-      );
+      ),
+    ]);
   }
 }
 
@@ -134,9 +175,7 @@ class CustomDrawer extends StatelessWidget {
         children: <Widget>[
           DrawerHeader(
             child: null,
-            decoration: BoxDecoration(
-              color: Colors.green
-            ),
+            decoration: BoxDecoration(color: Colors.green),
           ),
           ListTile(
             title: Text('item 1'),
@@ -155,5 +194,3 @@ class CustomDrawer extends StatelessWidget {
     );
   }
 }
-
-
