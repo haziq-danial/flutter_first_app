6bd5bd4447c8a94bef07f4aab7a8b445e5680525
haziq-danial <haziqdanial991@gmail.com>
2019-12-10 00:33:16 +0800

    some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -58,107 +58,105 @@ class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
         bottom: 0,
         right: _isCollapse ? 0 : -0.2 * _screenWidth,
         left: _isCollapse ? 0 : 0.7 * _screenWidth,
-        curve: Curves.ease,
+        curve: Curves.easeOutCubic,
         child: Material(
-          elevation: 8.0,
+          elevation: 8,
           child: Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white10,
+              leading: RawMaterialButton(
+                  elevation: 0,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isCollapse = !_isCollapse;
+                    });
+                  },
+                ),
+            ),
             body: Container(
                 child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 10, 10, 20),
-                    child: SafeArea(
-                      child: Row(
-                        children: <Widget>[
-                          RawMaterialButton(
-                            elevation: 0,
-                            shape: CircleBorder(),
-                            child: Icon(
-                              Icons.menu,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _isCollapse = !_isCollapse;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 100,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  child: Column(
                     children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Builder(
-                          builder: (context) => Form(
-                            key: _formKey,
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Column(
-                                children: <Widget>[
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                      labelText: 'Enter String',
-                                    ),
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Enter a string';
-                                      } else {
-                                        return null;
-                                      }
-                                    },
-                                    onSaved: (val) =>
-                                        setState(() => _stringName = val),
-                                    onTap: () {
-                                      final form = _formKey.currentState;
-                                      form.reset();
-                                      FocusScope.of(context).unfocus();
-                                    },
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 10.0),
-                                        child: RaisedButton(
-                                          color: Colors.green,
-                                          shape: StadiumBorder(),
-                                          child: Text(
-                                            'Submit',
-                                            style:
-                                                TextStyle(color: Colors.white),
+                      Container(
+                        height: _screenHeight / 1.5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                              child: Builder(
+                                builder: (context) => Form(
+                                  key: _formKey,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Column(
+                                      children: <Widget>[
+                                        TextFormField(
+                                          decoration: InputDecoration(
+                                            labelText: 'Enter String',
                                           ),
-                                          onPressed: () {
-                                            final form = _formKey.currentState;
-                                            if (form.validate()) {
-                                              form.save();
-                                              _showDialog(context, _stringName);
+                                          validator: (value) {
+                                            if (value.isEmpty) {
+                                              return 'Enter a string';
+                                            } else {
+                                              return null;
                                             }
                                           },
+                                          onSaved: (val) =>
+                                              setState(() => _stringName = val),
+                                          onTap: () {
+                                            final form = _formKey.currentState;
+                                            form.reset();
+                                            FocusScope.of(context).unfocus();
+                                          },
                                         ),
-                                      )
-                                    ],
-                                  )
-                                ],
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: <Widget>[
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 10.0),
+                                              child: RaisedButton(
+                                                color: Colors.green,
+                                                shape: StadiumBorder(),
+                                                child: Text(
+                                                  'Submit',
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                                onPressed: () {
+                                                  final form =
+                                                      _formKey.currentState;
+                                                  if (form.validate()) {
+                                                    form.save();
+                                                    _showDialog(
+                                                        context, _stringName);
+                                                  }
+                                                },
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ],
-              ),
-            )),
+                )),
           ),
         ),
       ),
@@ -170,6 +168,7 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      elevation: 0,
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
