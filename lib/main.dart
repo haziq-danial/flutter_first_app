3c18275be200909fbc1ff33b97e038dc8e2cadde
haziq-danial <haziqdanial991@gmail.com>
2019-12-11 12:52:13 +0800

    redo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -22,6 +23,7 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   String _stringName = '';
   bool _isCollapse = true;
 
@@ -50,117 +52,19 @@ class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
     final _screenHeight = _screenSize.height;
     final _screenWidth = _screenSize.width;
 
-    return Stack(children: <Widget>[
-      CustomDrawer(),
-      AnimatedPositioned(
-        duration: Duration(milliseconds: 300),
-        top: 0,
-        bottom: 0,
-        right: _isCollapse ? 0 : -0.2 * _screenWidth,
-        left: _isCollapse ? 0 : 0.7 * _screenWidth,
-        curve: Curves.easeOutCubic,
-        child: Material(
-          elevation: 8,
-          child: Scaffold(
-            appBar: AppBar(
-              elevation: 0,
-              backgroundColor: Colors.white10,
-              leading: RawMaterialButton(
-                  elevation: 0,
-                  child: Icon(
-                    Icons.menu,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _isCollapse = !_isCollapse;
-                    });
-                  },
-                ),
-            ),
-            body: Container(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        height: _screenHeight / 1.5,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                              child: Builder(
-                                builder: (context) => Form(
-                                  key: _formKey,
-                                  child: Align(
-                                    alignment: Alignment.center,
-                                    child: Column(
-                                      children: <Widget>[
-                                        TextFormField(
-                                          decoration: InputDecoration(
-                                            labelText: 'Enter String',
-                                          ),
-                                          validator: (value) {
-                                            if (value.isEmpty) {
-                                              return 'Enter a string';
-                                            } else {
-                                              return null;
-                                            }
-                                          },
-                                          onSaved: (val) =>
-                                              setState(() => _stringName = val),
-                                          onTap: () {
-                                            final form = _formKey.currentState;
-                                            form.reset();
-                                            FocusScope.of(context).unfocus();
-                                          },
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: <Widget>[
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      vertical: 10.0),
-                                              child: RaisedButton(
-                                                color: Colors.green,
-                                                shape: StadiumBorder(),
-                                                child: Text(
-                                                  'Submit',
-                                                  style: TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                                onPressed: () {
-                                                  final form =
-                                                      _formKey.currentState;
-                                                  if (form.validate()) {
-                                                    form.save();
-                                                    _showDialog(
-                                                        context, _stringName);
-                                                  }
-                                                },
-                                              ),
-                                            )
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white10,
+        actionsIconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(
+          color: Colors.black,
         ),
+        toolbarOpacity: 1.0,
       ),
-    ]);
+      drawer: CustomDrawer(),
+      body: Container(),
+    );
   }
 }
 
@@ -168,7 +72,6 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      elevation: 0,
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
