71c26d01bbbdfd9a8a9f3f86745768f585323c50
haziq-danial <haziqdanial991@gmail.com>
2019-12-12 11:47:49 +0800

    Created events page

---
+++ lib/events_page.dart
@@ -0,0 +1,38 @@
+import 'package:first_app/main.dart';
+import 'package:flutter/material.dart';
+import 'custom_drawer.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
+
+class EventsPage extends StatelessWidget {
+  static const String routeName = '/events';
+  final GlobalKey<InnerDrawerState> _innerDrawerKey =
+      GlobalKey<InnerDrawerState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return InnerDrawer(
+      key: _innerDrawerKey,
+      onTapClose: true,
+      swipe: true,
+      offset: IDOffset.only(bottom: 0, top: 0, right: 0, left: 0.5),
+      borderRadius: 20,
+      leftChild: CustomDrawer(pageName: 'Events',),
+      scaffold: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text('Events', style: TextStyle(color: Colors.black),),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              _innerDrawerKey.currentState
+                  .toggle(direction: InnerDrawerDirection.start);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(),
+        )
+      )
+    );
+  }
+}
