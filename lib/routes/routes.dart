852f09f564d8fff672681635f873207267d82feb
haziq-danial <haziqdanial991@gmail.com>
2019-12-12 11:49:51 +0800

    Added routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:first_app/main.dart';
+import 'package:first_app/events_page.dart';
+import 'package:first_app/notes_page.dart';
+
+class Routes {
+  static const String events = EventsPage.routeName;
+  static const String notes = NotesPage.routeName;
+}
