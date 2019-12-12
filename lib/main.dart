93c19cbbb2aab6859a8cfcb8d35cb9d98c1301ea
haziq-danial <haziqdanial991@gmail.com>
2019-12-12 15:55:02 +0800

    changed background, added pageview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:first_app/events_page.dart';
 import 'package:first_app/notes_page.dart';
 import 'package:first_app/routes/routes.dart';
@@ -11,21 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.teal,
-            appBarTheme: AppBarTheme(
-                color: Colors.white10,
-                iconTheme: IconThemeData(color: Colors.black),
-              )
-            ),
-        home: MyPage(),
-        routes: {
-          Routes.events: (context) => EventsPage(),
-          Routes.notes: (context) => NotesPage()
-        },
-      );
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.grey,
+          appBarTheme: AppBarTheme(
+              color: Colors.white10,
+              iconTheme: IconThemeData(color: Colors.white),
+              actionsIconTheme: IconThemeData(color: Colors.white))),
+      home: MyPage(),
+      routes: {
+        Routes.events: (context) => EventsPage(),
+        Routes.notes: (context) => NotesPage()
+      },
+    );
   }
 }
 
@@ -35,7 +36,6 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
-  
   final GlobalKey<FormState> _formKey = GlobalKey();
   final GlobalKey<InnerDrawerState> _innerDrawerKey =
       GlobalKey<InnerDrawerState>();
@@ -44,7 +44,6 @@ class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
@@ -79,87 +78,121 @@ class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
       swipe: true,
       offset: IDOffset.only(bottom: 0, top: 0, right: 0, left: 0.5),
       borderRadius: 20,
-      leftChild: CustomDrawer(pageName: 'Home',),
-      scaffold: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text('My First App', style: TextStyle(color: Colors.black),),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              _innerDrawerKey.currentState
-                  .toggle(direction: InnerDrawerDirection.start);
-            },
+      leftChild: CustomDrawer(
+        pageName: 'Home',
+      ),
+      scaffold: Stack(
+        children: <Widget>[
+          Container(
+            height: _screenHeight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1548291616-bfccc8db731d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80')),
+            ),
+            child: BackdropFilter(
+              filter: prefix0.ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+              ),
+            ),
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            height: _screenHeight / 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              title: OutlineButton(
+                borderSide: BorderSide(
+                    color: Colors.white, style: BorderStyle.solid, width: 1),
+                highlightedBorderColor: Colors.white30,
+                shape: StadiumBorder(),
+                padding: EdgeInsets.only(
+                    bottom: 2.0, top: 2.0, left: 40.0, right: 0.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'ACCOUNTS',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                onPressed: () {},
+              ),
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  _innerDrawerKey.currentState
+                      .toggle(direction: InnerDrawerDirection.start);
+                },
+              ),
+              actions: <Widget>[
+                RawMaterialButton(
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.lock_open),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          'LOGOUT',
+                          style: TextStyle(color: Colors.white, fontSize: 9),
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            body: SingleChildScrollView(
+                child: Column(
               children: <Widget>[
-                Builder(
-                  builder: (context) => Form(
-                    key: _formKey,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Column(
+                Container(
+                  height: _screenHeight / 4,
+                  width: _screenWidth,
+                  child: PageView(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.red,
+                        child: Stack(
                           children: <Widget>[
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Enter String',
-                              ),
-                              validator: (value) {
-                                if (value.isEmpty) {
-                                  return 'Enter a string';
-                                } else {
-                                  return null;
-                                }
-                              },
-                              onSaved: (val) =>
-                                  setState(() => _stringName = val),
-                              onTap: () {
-                                final form = _formKey.currentState;
-                                form.reset();
-                                FocusScope.of(context).unfocus();
-                              },
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 10.0),
-                                  child: RaisedButton(
-                                    color: Colors.teal,
-                                    shape: StadiumBorder(),
-                                    child: Text(
-                                      'Submit',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      final form = _formKey.currentState;
-                                      if (form.validate()) {
-                                        form.save();
-                                        _showDialog(context, _stringName);
-                                      }
-                                    },
-                                  ),
-                                )
-                              ],
+                            Positioned(
+                              top: _screenHeight / 14,
+                              left: _screenWidth / 3,
+                              child: Text(
+                                'RM 422',
+                                style: TextStyle(
+                                  color: Colors.white, 
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w100,
+                                ),
+                              )
                             )
                           ],
                         ),
                       ),
-                    ),
+                      Container(
+                        color: Colors.blue,
+                      ),
+                      Container(
+                        color: Colors.green,
+                      ),
+                    ],
                   ),
-                )
+                ),
               ],
-            ),
+            )),
           ),
-        ),
+        ],
       ),
     );
   }
